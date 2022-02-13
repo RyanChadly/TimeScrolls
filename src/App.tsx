4bf82dcc37768d3dc7a5cad46bb9adf36f02da86
Chadly, Ryan <rchadly@microstrategy.com>
2022-02-13 09:07:33 +0100

    Make the slider work

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
 import { useState, useEffect, useRef } from "react";
+import Hour from "./components/Hour";
 
 export default function App() {
   const containerRefDiv = useRef() as React.MutableRefObject<HTMLDivElement>;
-
   const [width, setWidth] = useState(0);
   const [currentScrollLeft, setCurrentScrollLeft] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-  const updateDivWidth = () => {
+  const handleScroll = (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
     const newScrollLeft = containerRefDiv.current.scrollLeft;
-    if (currentScrollLeft < newScrollLeft) {
-      setCurrentScrollLeft(newScrollLeft);
-      if (width === 0) {
-        setWidth(containerRefDiv.current.clientWidth + 10);
-      } else {
-        setWidth((previous) => previous + 10);
-      }
+    const maxScrollLeft =
+      containerRefDiv.current.scrollWidth - containerRefDiv.current.clientWidth;
+    if (maxScrollLeft - 1 <= newScrollLeft) {
+      containerRefDiv.current.scrollLeft = maxScrollLeft - 1;
+      setCounter((previous) => previous + 1);
+    }
+    if (newScrollLeft === 0) {
+      containerRefDiv.current.scrollLeft = newScrollLeft + 1;
+      setCounter((previous) => previous - 1);
     }
   };
-
   useEffect(() => {
-    console.log("new width set: ", width);
-  }, [width]);
-
+    containerRefDiv.current.scrollLeft = 1;
+  }, []);
   const getInnerDivStyle = () => {
     if (containerRefDiv.current && width !== 0) {
       return `${width}px`;
@@ -34,13 +35,27 @@ export default function App() {
     <div>
       <div
         className="App"
-        style={{ overflowX: "scroll", width: "100%" }}
+        style={{
+          overflowX: "scroll",
+          width: "100%",
+          border: "solid",
+          height: "200",
+          // scrollBehavior: "smooth",
+        }}
         ref={containerRefDiv}
-        onScroll={updateDivWidth}
+        onScroll={handleScroll}
       >
-        <div style={{ width: getInnerDivStyle() }}>{width}</div>
+        <div
+          style={{
+            float: "none",
+            width: "101%",
+            border: "solid",
+            textAlign: "center",
+          }}
+        >
+          {counter}
+        </div>
       </div>
-      <div>{width}</div>
     </div>
   );
 }
