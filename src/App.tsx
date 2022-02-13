5160547261e87bb011c5c0582c7b6b8b5037e3e0
Chadly, Ryan <rchadly@microstrategy.com>
2022-02-13 09:56:51 +0100

    Move slider out of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,19 @@
 import { useState, useEffect, useRef } from "react";
-import Hour from "./components/Hour";
+import Slider from "./components/Slider";
 
 export default function App() {
-  const containerRefDiv = useRef() as React.MutableRefObject<HTMLDivElement>;
-  const [width, setWidth] = useState(0);
-  const [currentScrollLeft, setCurrentScrollLeft] = useState(0);
-  const [counter, setCounter] = useState(0);
-
-  const handleScroll = (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
-    const newScrollLeft = containerRefDiv.current.scrollLeft;
-    const maxScrollLeft =
-      containerRefDiv.current.scrollWidth - containerRefDiv.current.clientWidth;
-    if (maxScrollLeft - 1 <= newScrollLeft) {
-      containerRefDiv.current.scrollLeft = maxScrollLeft - 1;
-      setCounter((previous) => previous + 1);
-    }
-    if (newScrollLeft === 0) {
-      containerRefDiv.current.scrollLeft = newScrollLeft + 1;
-      setCounter((previous) => previous - 1);
-    }
+  const [count, setCount] = useState(0);
+  const handleSlide = (value: number) => {
+    setCount((previous) => previous + value);
   };
-  useEffect(() => {
-    containerRefDiv.current.scrollLeft = 1;
-  }, []);
-  const getInnerDivStyle = () => {
-    if (containerRefDiv.current && width !== 0) {
-      return `${width}px`;
-    } else {
-      return "101%";
-    }
-  };
-
   return (
     <div>
-      <div
-        className="App"
-        style={{
-          overflowX: "scroll",
-          width: "100%",
-          border: "solid",
-          height: "200",
-          // scrollBehavior: "smooth",
-        }}
-        ref={containerRefDiv}
-        onScroll={handleScroll}
-      >
-        <div
-          style={{
-            float: "none",
-            width: "101%",
-            border: "solid",
-            textAlign: "center",
-          }}
-        >
-          {counter}
-        </div>
-      </div>
+      <button onClick={() => setCount(0)}>Reset</button>
+      <Slider count={count} handleSlide={handleSlide} />
+      <Slider count={count} handleSlide={handleSlide} />
+      <Slider count={count} handleSlide={handleSlide} />
+      <Slider count={count} handleSlide={handleSlide} />
+      <Slider count={count} handleSlide={handleSlide} />
     </div>
   );
 }
