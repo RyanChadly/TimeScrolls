c9852472fd9ba19f549b46d6b20702d97771ae17
Chadly, Ryan <chadlyryan@gmail.com>
2022-04-23 22:59:38 +0200

    Move cookies functions to utils

--- src/components/Save/Save.tsx
+++ src/components/Save/Save.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Location } from "../../App";
 import "./Save.css";
 import { isEqual } from "lodash";
+import { getCookie, setCookie } from "../../utils/cookies";
 interface SaveProps {
   locations: Location[];
 }
@@ -39,25 +40,3 @@ const Save: React.FC<SaveProps> = ({ locations }) => {
 };
 
 export default Save;
-
-function setCookie(cname: string, cvalue: string, exdays: number) {
-  const d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  let expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname: string) {
-  let name = cname + "=";
-  let ca = document.cookie.split(";");
-  for (const element of ca) {
-    let c = element;
-    while (c.charAt(0) === " ") {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) === 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
