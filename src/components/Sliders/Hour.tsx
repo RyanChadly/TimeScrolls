967e02f4f78a878dcfd12e43318f6cf016cbe701
Chadly, Ryan <chadlyryan@gmail.com>
2022-10-20 22:29:35 +0200

    Update Hour.tsx

--- src/components/Sliders/Hour.tsx
+++ src/components/Sliders/Hour.tsx
@@ -9,34 +9,23 @@ interface Props {
 }
 
 const Hour: React.FC<Props> = ({ hour, minutes, current, colorName }) => {
-  const getColor = () => {
-    return colors.find((color) => color.name === colorName);
-  };
+  const color = colors.find((c) => c.name === colorName);
+  const day = range(9, 17);
 
-  const getStyle = () => {
-    const day = range(9, 17);
-    if (day.includes(hour)) {
-      return {
-        color: getColor()?.dark,
-        background: `linear-gradient(170deg, white 0%, ${
-          getColor()?.lighter
-        }55 100%)`,
+  const getStyle = day.includes(hour)
+    ? {
+        color: color?.dark,
+        background: `linear-gradient(170deg, white 0%, ${color?.lighter}55 100%)`,
+      }
+    : {
+        background: `linear-gradient(170deg, ${color?.lighter}55 0%, ${color?.lighter} 100%)`,
+        color: color?.dark,
       };
-    } else {
-      return {
-        background: `linear-gradient(170deg, ${getColor()?.lighter}55 0%, ${
-          getColor()?.lighter
-        } 100%)`,
-        color: getColor()?.dark,
-        // fontSize: "large",
-      };
-    }
-  };
 
   return (
     <span
       className={`hour  ${current ? "current" : ""} `}
-      style={{ ...getStyle(), flex: minutes }}
+      style={{ ...getStyle, flex: minutes }}
       id={`h${hour}`}
     >
       {hour}
